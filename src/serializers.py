308ddaebdd3edebe741e26ceddc97531890505ac
Erik <erik.n.lewis@gmail.com>
2022-10-13 15:36:47 -0700

    Added JsonSerializer and associated test.

--- src/serializers.py
+++ src/serializers.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 class AnimalFactory:
     def __init__(self):
@@ -19,7 +19,31 @@ class AnimalFactory:
             raise ValueError(format)
 
 class Serializer(ABC):
-    pass
+    
+    @abstractmethod
+    def add_param(self, key: str, val: str):
+        pass
+
+    def to_str(self) -> str:
+        pass
+
+
+class JsonSerializer(Serializer):
+
+    def __init__(self) -> None:
+        self._data = {}
+
+    def __call__(self, **kwargs):
+        for key, val in kwargs.items():
+            self.add_param(key, val)
+        return self._data
+
+    def add_param(self, key: str, val: str):
+        self._data[key] = val
+    
+    def to_str(self) -> str:
+        return json.dumps(self._data)
+
 
 class Animal(ABC):
     pass
--- tests/serializer_test.py
+++ tests/serializer_test.py
@@ -1,10 +1,11 @@
 import pytest
-from dataclasses import dataclass
+import json
+from dataclasses import dataclass, asdict
 
-from serializers import AnimalFactory
+from serializers import AnimalFactory, JsonSerializer
 
 @dataclass
-class fake_animal:
+class fake_animal_class:
     name: str
     phone: str
     address: str
@@ -20,10 +21,10 @@ def register_factory(factory):
 
 @pytest.fixture
 def fake_animal():
-    return fake_animal("Erik", "4033321374", "123 Gelmer St")
+    return fake_animal_class("Erik", "4033321374", "123 Gelmer St")
     
 
-def serialize_to_json(factory, register_factory, fake_animal):
-    json_animal = factory.create_animal("JSON", **fake_animal.asdict())
+def test_serialize_to_json(factory, register_factory, fake_animal):
+    json_animal = factory.create_animal("JSON", **asdict(fake_animal))
 
-    assert json_animal.to_str() == str(fake_animal.asdict())
+    assert json.dumps(json_animal) == json.dumps(asdict(fake_animal))
