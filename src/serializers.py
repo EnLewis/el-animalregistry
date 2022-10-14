a81b5c0d5e604a75e9c2a929a7e1cc6e55deb6c2
Erik <erik.n.lewis@gmail.com>
2022-10-13 17:26:22 -0700

    Added XML serializer

--- src/serializers.py
+++ src/serializers.py
@@ -1,5 +1,7 @@
 import json
 import yaml
+import random
+import xml.etree.ElementTree as element_tree
 from typing import Any, Callable
 
 from abc import ABC, abstractmethod
@@ -55,6 +57,23 @@ class YamlSerializer(JsonSerializer):
     def to_str(self) -> Callable:
         return yaml.dump
 
+class XmlSerializer(Serializer):
+    def __init__(self):
+        self._data = element_tree.Element("animal", attrib={'id': str(random.randint(0,10))})
+    
+    def __call__(self, **kwargs):
+        for key, val in kwargs.items():
+            self.add_param(key, val)
+        animal = XmlAnimal(self._data, self.to_str())
+        return animal
+    
+    def add_param(self, key, value):
+        prop = element_tree.SubElement(self._data, key)
+        prop.text = str(value)
+    
+    def to_str(self):
+        return element_tree.tostring
+
 @dataclass
 class Animal():
     data: Any
@@ -62,3 +81,8 @@ class Animal():
 
     def __str__(self):
         return self.to_str_method(self.data)
+
+@dataclass 
+class XmlAnimal(Animal):
+    def __str__(self):
+        return self.to_str_method(self.data, encoding='unicode')
--- tests/serializer_test.py
+++ tests/serializer_test.py
@@ -3,7 +3,7 @@ import json
 import yaml
 from dataclasses import dataclass, asdict
 
-from serializers import AnimalFactory, JsonSerializer, YamlSerializer
+from serializers import AnimalFactory, JsonSerializer, XmlSerializer, YamlSerializer
 
 @dataclass
 class fake_animal_class:
@@ -20,10 +20,14 @@ def factory():
 def register_factory(factory):
     factory.register_format("JSON", JsonSerializer())
     factory.register_format("YAML", YamlSerializer())
+    factory.register_format("XML", XmlSerializer())
 
 @pytest.fixture
 def fake_animal():
     return fake_animal_class("Erik", "4033321374", "123 Gelmer St")
+
+def validate_xml(xml):
+    return type(str(xml)) is str
 
 def test_serialize_to_json(factory, register_factory, fake_animal):
     json_animal = factory.create_animal("JSON", **asdict(fake_animal))
@@ -34,6 +38,13 @@ def test_serialize_to_yaml(factory, register_factory, fake_animal):
     yaml_animal = factory.create_animal("YAML", **asdict(fake_animal))
 
     assert str(yaml_animal) == yaml.dump(asdict(fake_animal))
+
+def test_serialize_to_xml(factory, register_factory, fake_animal):
+    xml_animal = factory.create_animal("XML", **asdict(fake_animal))
+    
+    # This is a tricky one to test since xml format takes a lot of setup
+    # For now we will just ensure that we get a string back with a stub
+    assert validate_xml(xml_animal)
 
 def test_serialize_json_to_yaml(factory, register_factory, fake_animal):
     # Swap serializer
